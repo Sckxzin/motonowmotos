98cb747138ee76eed41755f74d734b2068c29912
Sckxzin <kaykylz069@gmail.com>
2025-12-23 14:56:53 -0300

    Update motos.js

--- routes/motos.js
+++ routes/motos.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
-// LISTAR TODAS AS MOTOS
-// usado pela tela de estoque
 router.get("/", async (req, res) => {
   try {
     const result = await pool.query(`
@@ -16,12 +14,12 @@ router.get("/", async (req, res) => {
         santander,
         data_entrada
       FROM motos
+      WHERE status = 'ESTOQUE'
       ORDER BY cidade, modelo
     `);
 
     res.json(result.rows);
   } catch (err) {
-    console.error("Erro ao buscar motos:", err);
     res.status(500).json({ erro: "Erro ao buscar motos" });
   }
 });
