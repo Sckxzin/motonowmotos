e6ad187a7be5414741277d6e61a57f0c12ab3d81
Sckxzin <kaykylz069@gmail.com>
2025-12-23 09:56:12 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,9 @@ app.get("/", (req, res) => {
   res.send("MotoNow backend online 🚀");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Backend rodando na porta", PORT);
 });
+
