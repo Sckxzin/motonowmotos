24c64a29ec5b9caf7d1f053c89fe30e94ce8ff30
Sckxzin <kaykylz069@gmail.com>
2025-12-23 10:51:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const db = require("./db");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-/* ================= TESTE DE BANCO ================= */
-db.query("select 1")
-  .then(() => console.log("✅ Postgres conectado com sucesso"))
-  .catch(err => console.error("❌ Erro ao conectar no Postgres:", err));
-
-/* ================= ROTAS ================= */
 app.get("/", (req, res) => {
-  res.json({ status: "Backend MotoNow OK 🚀" });
+  res.json({ status: "Backend MotoNow OK" });
 });
 
-/* ================= START ================= */
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("🚀 Backend rodando na porta", PORT);
