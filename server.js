603661ec94834f1a8ea3e02b0ad025772f3c31f0
Sckxzin <kaykylz069@gmail.com>
2025-12-23 13:43:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
-const loginRoutes = require("./routes/login");
 const motosRoutes = require("./routes/motos");
 const vendasRoutes = require("./routes/vendas");
+const transferenciasRoutes = require("./routes/transferencias");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.use("/login", loginRoutes);
-app.use("/motos", motosRoutes);
-app.use("/vendas", vendasRoutes);
-
 app.get("/", (req, res) => {
-  res.json({ status: "Backend MotoNow OK + Postgres conectado" });
+  res.json({ status: "MotoNow backend online 🚀" });
 });
 
-const PORT = process.env.PORT || 8080;
+app.use("/motos", motosRoutes);
+app.use("/vendas", vendasRoutes);
+app.use("/transferencias", transferenciasRoutes);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("🚀 Backend rodando na porta", PORT);
 });
