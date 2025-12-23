0301dc97b6df0c249e342488b5505168289b96ab
Sckxzin <kaykylz069@gmail.com>
2025-12-23 11:04:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ status: "Backend MotoNow OK" });
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT 1 AS ok");
+    res.json({
+      status: "Backend MotoNow OK",
+      db: result.rows[0]
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ erro: "Erro ao conectar no banco" });
+  }
 });
 
 const PORT = process.env.PORT || 8080;
