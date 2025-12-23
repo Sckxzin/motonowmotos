be7171ba34f4e9fa1c1b713cadecb44321fd5106
Sckxzin <kaykylz069@gmail.com>
2025-12-23 09:45:10 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,5 @@
-const oracledb = require("oracledb");
-
-// usar OBJETO (mais limpo)
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-
 async function conectar() {
-  return await oracledb.getConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    connectString: process.env.DB_CONNECTION_STRING
-  });
+  return null;
 }
 
 module.exports = conectar;
