65624c1760dd3ce934165f6e2a06db79687e4f50
Sckxzin <kaykylz069@gmail.com>
2025-12-23 12:16:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
+
+const loginRoutes = require("./routes/login");
+const motosRoutes = require("./routes/motos");
+const vendasRoutes = require("./routes/vendas");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// conexão Postgres via Railway
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+app.use("/login", loginRoutes);
+app.use("/motos", motosRoutes);
+app.use("/vendas", vendasRoutes);
 
-app.get("/", async (req, res) => {
-  try {
-    await pool.query("SELECT 1");
-    res.json({ status: "Backend MotoNow OK + Postgres conectado" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ erro: "Erro ao conectar no banco" });
-  }
+app.get("/", (req, res) => {
+  res.json({ status: "Backend MotoNow OK + Postgres conectado" });
 });
 
 const PORT = process.env.PORT || 8080;
